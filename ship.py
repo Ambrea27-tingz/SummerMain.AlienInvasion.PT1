a6c739d77fb43c5fbc5cdc6058a1564cf9d8bfea
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-07-11 19:51:33 -0400

    Refactored game loop for events and drawing.

--- ship.py
+++ ship.py
@@ -1,25 +1,39 @@
 import pygame
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from alien_invasion import AlienInvasion
 
 class Ship:
-
-
-    def __init__(self, game: 'AlienInvasion'):
-        self.game = game
-        self.settings = game.settings
-        self.screen = game.screen
-        self.screen_rect = self.screen.get_rect()
-
-        self.image = pygame.image.load(self.settings.ship_file)
-        self.image = pygame.transform.scale(self.image, 
-             (self.settings.ship_w, self.settings.ship_h)
-             )
-        
+    """A class to manage the ship."""
+    def __init__(self, ai_game):
+        """Start the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+       
+       
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+        #Generate each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-def draw(self):
-    self.screen.blit(self.image, self.rect)
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flag; start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flags."""
+         # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right: 
+           self.x += self.settings.ship_speed
+         
+        if self.moving_left and self.rect.left > 0:    
+            self.x -= self.settings.ship_speed
+      
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
